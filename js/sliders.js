714c7a11ba51a5f67c881ea34a2b02c9c4050c6c
atiii <aty.sokhansanj@gmail.com>
2020-10-17 14:02:40 +0330

    26/7/99 v01

--- js/sliders.js
+++ js/sliders.js
@@ -1,90 +1,84 @@
 $(document).ready(function () {
 
 
-    $(".img-gallery").owlCarousel( {
+  $(".img-gallery").owlCarousel({
 
-        rtl: true,
-        nav: false,
+    rtl: true,
+    nav: false,
+    items: 3,
+    dots: true,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    loop: true,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        items: 1,
+      }
+
+      ,
+      768: {
         items: 3,
-        dots: true,
-        center: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        loop: true,
-        lazyLoad: true,
-        responsive: {
-          0: {
-            items: 1,
-          }
-          ,
-          768: {
-            items: 3,
-          }
-          ,
-        }
-
-        ,
       }
 
-    );
+      ,
+    }
 
-    $(".video-gallery").owlCarousel( {
-        rtl: true,
-        nav: true,
-        items: 1,
-        dots: false,
-        center: true,
-        video: true,
-        navText: [ '<i class="fa fa-angle-left"></i>',
-        '<i class="fa fa-angle-right"></i>',
-        ],
-      }
-
-    );
-
-    $(".cards-gallery").owlCarousel( {
-
-        rtl: true,
-        nav: true,
-        items: 1,
-        // margin:16,
-        // autoplay: true,
-        autoplayTimeout: 5000,
-        navText: [ '<i class="fas fa-arrow-left"></i>',
-        '<i class="fas fa-arrow-right"></i>',
-        ],
-        responsive: {
-          0: {
-            items: 1,
-            nav: true
-          }
-          ,
-          576: {
-            loop: true,
-            items: 2,
-            nav: true
-          }
-          ,
-          768: {
-            loop: true,
-            items: 3,
-            nav: true
-          }
-
-          ,
-          1100: {
-            items: 4,
-            nav: true
-          }
-          ,
-        }
-
-        ,
-
-      }
-
-    );
-
+    ,
   }
+
+  );
+
+  $(".video-gallery").owlCarousel({
+    rtl: true,
+    nav: true,
+    items: 1,
+    dots: false,
+    center: true,
+    video: true,
+    navText: ['<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+  }
+
+  );
+
+  $(".cards-gallery").owlCarousel({
+    rtl: true,
+    nav: true,
+    items: 1,
+    // margin: 16,
+    loop: true,
+    // autoplay: true,
+    autoplayTimeout: 5000,
+    navText: [
+      '<i class="fas fa-arrow-left"></i>',
+      '<i class="fas fa-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+
+      576: {
+        items: 2,
+        nav: true,
+      },
+
+      768: {
+        items: 2,
+        nav: true,
+      },
+
+      991: {
+        items: 3,
+        nav: true,
+      },
+    },
+  });
+
+}
 
 )
